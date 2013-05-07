ab871fa300ee82f2e0444a2d878aeebec7fc4e7a
Pablo ALINGERY <palinger@sol-palinger.(none)>
2013-05-07 18:11:21 +0200

    GAIA-DEM python client

--- gaia_client_idoc.py
+++ gaia_client_idoc.py
@@ -2,7 +2,7 @@
 """
 This script has been designed to give python programmers an easy way to interrogate medoc gaia-dem sitools2 interface.
 You can make a search providing a date range and get your selection very easilly.
-@author: Pablo ALINGERY for IAS 07-03-2013
+@author: Pablo ALINGERY for IAS 07-05-2013
 """
 __version__ = "1.0"
 __license__ = "GPL"
