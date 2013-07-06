75d0f22f8660541c9e57dc5a81b6b25678c35566
Pablo IAS <pablo.alingery@ias.u-psud.fr>
2013-07-06 04:15:03 +0200

    hide get_selection() for user

--- examples/example_media.py
+++ examples/example_media.py
@@ -26,17 +26,20 @@ from sitools2.clients.sdo_client_medoc import *
 d1 = datetime(2011,01,01,0,0,0)
 d2 = d1 + timedelta(minutes=20)
 
-sdo_data_list = search( DATES=[d1,d2], WAVES=['335','304'], CADENCE=['1 min'] ) 
+sdo_data_list = media_search( DATES=[d1,d2], WAVES=['335','304'], CADENCE=['1 min'] , NB_RES_MAX= 5) 
+
+#for sdo_data_item in sdo_data_list :
+#	print sdo_data_item
 
 #To limit the results sent by the server set nb_res_max
-#sdo_data_list = search( DATES=[d1,d2], WAVES=['335','304'], nb_res_max= 5 ,CADENCE=['1 min'] ) 
+#sdo_data_list = search( DATES=[d1,d2], WAVES=['335','304'], NB_RES_MAX= 5 ,CADENCE=['1 min'] ) 
 
 #The fastest way to retrieve data
 #PS : The directory 'results' has to be created !
-#get (MEDIA_DATA_LIST=sdo_data_list,TARGET_DIR='/tmp', DECOMPRESS=True)
+#media_get (MEDIA_DATA_LIST=sdo_data_list,TARGET_DIR='/tmp', DECOMPRESS=True)
 
 #Need to get a tar ball or zip file do sthg like :
-get_selection(MEDIA_DATA_LIST=sdo_data_list, TARGET_DIR="/tmp" ,FILENAME="my_download_file")
+media_get(MEDIA_DATA_LIST=sdo_data_list, TARGET_DIR="/tmp" , DOWNLOAD_TYPE="tar", FILENAME="my_download_file")
 
 #And if you want to specifies files name do sthg like 
 #for item in sdo_data_list :
