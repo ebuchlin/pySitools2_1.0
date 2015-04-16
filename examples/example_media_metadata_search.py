6999245dbac28b7ddf58a9cabea0a33b41dcf483
MedocIAS <medoc@ias.u-psud.fr>
2015-04-16 15:37:06 +0200

    Exemple d 'utilisation de media_metadata_search()

---
+++ examples/example_media_metadata_search.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+"""
+
+@author: Pablo ALINGERY 
+"""
+
+from  sitools2.clients.sdo_client_medoc import *
+
+d1 = datetime(2011,01,01,0,0,0)
+d2 = d1 + timedelta(minutes=15)
+
+sdo_data_list = media_search( DATES=[d1,d2], WAVES=['335','193'], CADENCE=['1 min'], nb_res_max=2 ) 
+
+recnum_list = []
+for item in sdo_data_list :
+	recnum_list.append(item.recnum)
+
+print recnum_list
+meta=media_metadata_search(KEYWORDS=['date__obs','quality','cdelt1','cdelt2','crval1'], RECNUM_LIST=recnum_list)
+
+print meta
+
