b752d8c7775657b81fb7dbabddc5697cd74f4e6d
MedocIAS <medoc@ias.u-psud.fr>
2015-04-16 16:34:56 +0200

    list of string expected for RECNUM_LIST

--- examples/example_media_metadata_search.py
+++ examples/example_media_metadata_search.py
@@ -13,7 +13,7 @@ sdo_data_list = media_search( DATES=[d1,d2], WAVES=['335','193'], CADENCE=['1 mi
 
 recnum_list = []
 for item in sdo_data_list :
-	recnum_list.append(item.recnum)
+	recnum_list.append(str(item.recnum))
 
 print recnum_list
 meta=media_metadata_search(KEYWORDS=['date__obs','quality','cdelt1','cdelt2','crval1'], RECNUM_LIST=recnum_list)
