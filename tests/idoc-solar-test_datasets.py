e97d4bcb206ca03b145b65c896d25d1613ebef21
MedocIAS <medoc@ias.u-psud.fr>
2016-01-10 23:04:52 -0400

    Test idoc-solar-test solar portail

---
+++ tests/idoc-solar-test_datasets.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+#    SITools2 client for Python
+#    Copyright (C) 2013 - Institut d'astrophysique spatiale
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses
+
+__author__="Pablo ALINGERY"
+__date__ ="$8 janvier 2015 19:38:58$"
+
+import unittest
+from sitools2.core.pySitools2 import *
+
+class TestSitools2Core(unittest.TestCase):        
+    
+    def setUp(self):
+        pass
+    
+    def testNbProjects(self):
+        sitools2 = Sitools2Instance('http://idoc-solar-test.ias.u-psud.fr')
+        projects = sitools2.list_project()
+#	print projects[0].dataset_list()
+no	print projects[1].dataset_list()
+        self.assertEqual( len(projects), 2)           
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
