1ca5833aeebb052952b622a0fa9f8322be81a281
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2016-10-07 11:00:55 +0200

    Print data after research

--- examples/example_idoc-solar-portal_hmi_data.py
+++ examples/example_idoc-solar-portal_hmi_data.py
@@ -11,14 +11,13 @@ d2 = d1 + timedelta(minutes=120)
 
 sdo_hmi_data_list = media_search( DATES=[d1,d2], SERIE='hmi.sharp_cea_720s_nrt', CADENCE=['1 h'], nb_res_max=10 ) 
 
-#Metadata info 
-#Build recnum list
+
 recnum_list = []
 for item in sdo_hmi_data_list :
-	#print local path 
-	print item.ias_location
+	print item
 	recnum_list.append(str(item.recnum))
 
+#Metadata info 
 print recnum_list
 meta=media_metadata_search(KEYWORDS=['date__obs','quality','cdelt1','cdelt2','crval1'],SERIE='hmi.sharp_cea_720s_nrt', RECNUM_LIST=recnum_list)
 print meta
