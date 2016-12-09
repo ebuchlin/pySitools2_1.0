50fd82965ee79fecac23dd6c2b4b9de0663f0063
MedocIAS <medoc@ias.u-psud.fr>
2016-12-09 14:02:33 +0100

    print() for python3

--- examples/example_idoc-solar-portal_hmi_data.py
+++ examples/example_idoc-solar-portal_hmi_data.py
@@ -6,8 +6,8 @@
 
 from  sitools2.clients.sdo_client_medoc import *
 
-d1 = datetime(2016,01,01,0,0,0)
-d2 = datetime(2016,06,01,0,0,0)
+d1 = datetime(2016,1,1,0,0,0)
+d2 = datetime(2016,6,1,0,0,0)
 
 sdo_hmi_data_list = media_search( DATES=[d1,d2], SERIES='hmi.sharp_cea_720s_nrt', CADENCE=['1d'], nb_res_max=100 ) 
 
@@ -15,9 +15,9 @@ sdo_hmi_data_list = media_search( DATES=[d1,d2], SERIES='hmi.sharp_cea_720s_nrt'
 recnum_list=[item.recnum for item in sdo_hmi_data_list]
 
 #Metadata info 
-print recnum_list
+print (recnum_list)
 meta=media_metadata_search(KEYWORDS=['date__obs','quality','cdelt1','cdelt2','crval1'],SERIES='hmi.sharp_cea_720s_nrt', RECNUM_LIST=recnum_list)
-print meta
+print (meta)
 
 #Download data 
 for data in sdo_hmi_data_list :
