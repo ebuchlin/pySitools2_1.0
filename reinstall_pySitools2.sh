8e04bff689c0750690e1ebece8aa6133bb3cf5d2
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2017-01-27 17:31:44 +0100

    Remove old egg and use pip

--- reinstall_pySitools2.sh
+++ reinstall_pySitools2.sh
@@ -2,8 +2,10 @@
 
 #Old 
 #sudo easy_install -m pySitools2_1.0 
-#sudo rm -Rf /usr/local/lib/python2.7/dist-packages/pySitools2_1.0-0.1-py2.7.egg
-#sudo rm -Rf ~/.local/lib/python2.7/site-packages/pySitools2_1.0-0.1-py2.7.egg
+sudo rm -Rf /usr/local/lib/python2.*/dist-packages/pySitools2_1.0-0.1-py2.*.egg
+sudo rm -Rf ~/.local/lib/python2.*/site-packages/pySitools2_1.0-0.1-py2.*.egg
+sudo rm -Rf /usr/local/lib/python3.*/dist-packages/pySitools2_1.0-0.1-py3.*.egg
+sudo rm -Rf ~/.local/lib/python3.*/site-packages/pySitools2_1.0-0.1-py3.*.egg
 #sudo python setup.py install 
 
 #New 24-01-2017 Avoid easy_install issue repported by A.Beeeleme 
