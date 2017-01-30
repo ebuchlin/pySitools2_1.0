eacb2d5065e55df54ae4898a474abf0a91003704
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2017-01-30 15:20:18 +0100

    improve with pip capabilities

--- reinstall_pySitools2.sh
+++ reinstall_pySitools2.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-#Old 
-#sudo easy_install -m pySitools2_1.0 
-sudo rm -Rf /usr/local/lib/python2.*/dist-packages/pySitools2_1.0-0.1-py2.*.egg
-sudo rm -Rf ~/.local/lib/python2.*/site-packages/pySitools2_1.0-0.1-py2.*.egg
-sudo rm -Rf /usr/local/lib/python3.*/dist-packages/pySitools2_1.0-0.1-py3.*.egg
-sudo rm -Rf ~/.local/lib/python3.*/site-packages/pySitools2_1.0-0.1-py3.*.egg
-#sudo python setup.py install 
+source ~/.bashrc
 
 #New 24-01-2017 Avoid easy_install issue repported by A.Beeeleme 
-sudo -H pip uninstall pySitools2_1.0
-sudo -H pip install --force-reinstall . 
+#sudo pip uninstall -y pySitools2-1.0
+sudo -H pip install --upgrade .
