1b14508ab8f5411b3059f8ac5ea4e619e709c256
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2018-11-27 11:58:17 +0100

    Test stereo data research download ok on idoc-medoc, ko on idoc-medoc-test, reference to idoc-medoc-test on idoc-medoc

---
+++ examples/example_idoc-medoc_test_stereo.py
@@ -0,0 +1,104 @@
+#! /usr/bin/python
+"""
+
+"""
+
+from sitools2.core.pySitools2 import *
+from sitools2.clients import constants
+
+
+sitools_url = constants.SITOOLS2_URL
+
+def main():
+
+    print("Loading SitoolsClient for", sitools_url)
+    SItools1 = Sitools2Instance(sitools_url)
+    #print ("SItools1 : %s " % SItools1.list_project())
+
+
+
+    ds1 = Dataset(sitools_url + "/webs_STEREO_dataset")
+    ds1.display()
+
+    #date__ob
+    #Format must be something like 2015-11-01T00:00:00.000 in version Sitools2 3.0 that will change
+    param_query1 = [
+                      [ds1.fields_dict['date_obs']],
+                      ['2010-06-01T00:00:00.000', '2010-06-02T00:00:00.000'],
+                      'DATE_BETWEEN'
+    ]
+
+    Q1 = Query(param_query1)
+    #Q2 = Query(param_query2)
+    #Q3 = Query(param_query3)
+
+    #Ask recnum, sunum,series_name,date__obs, ias_location,ias_path
+
+    O1 = [
+        ds1.fields_dict['filename'], ds1.fields_dict['filesize'],
+        ds1.fields_dict['instrument'], ds1.fields_dict['date_obs'],
+        ds1.fields_dict['secchisata'], ds1.fields_dict['secchisatb'],
+        ds1.fields_dict['wavemin'], ds1.fields_dict['wavemax'],
+        ds1.fields_dict['datatype'],  ds1.fields_dict['download_path'],
+         ds1.fields_dict['path'],  ds1.fields_dict['id_sitools_view']
+    ]
+
+    #Sort date__obs ASC
+    S1 = [[ds1.fields_dict['date_obs'], 'ASC']]
+
+    #       for field in ds1.fields_dict :
+    #               field.display()
+
+    #        print "\nPrint Query  ..."
+    Q1.display()
+    # Q2.display()
+    # Q3.display()
+
+    #result = ds1.search([Q1], O1, S1)
+#    print(len(result), " item(s) found\n")
+    result=ds1.search([Q1],O1,S1,limit_to_nb_res_max=10)
+    print(len(result), " item(s) found\n")
+    if len(result) != 0:
+          print("Results :\n")
+          for i, data in enumerate(result):
+              print("%d) %s" % (i + 1, data))
+
+
+
+    print ("Download just one STEREO data\nIn progress please wait ...")
+    print("item : \n%s" % result[1])
+    print("id : %s" % result[1]['id_sitools_view'])
+    try :
+          ds1.execute_plugin(
+             plugin_name='pluginSTEREOtar',
+             pkey_values_list=[result[1]['id_sitools_view']],
+             FILENAME='first_download_STEREO.tar'
+         )
+    except ValueError as e:
+        print ("Issue downloading id_sitools_view : %s " % result[1]['id_sitools_view'])
+        print ("type is: %s" % e.__class__.__name__)
+        print ("Message : %s" % e.message)
+    except Exception as e:
+        print ("Issue downloading id_sitools_view : %s " % result[1]['id_sitools_view'])
+        print ("type is: %s" % e.__class__.__name__)
+        print ("Message : %s" % e.message)
+
+    else :
+        print ("Download id_sitools_view : %s ,file %s completed" % (result[1]['id_sitools_view'],
+                                                                     'first_download_STEREO.tar'))
+
+
+    print("Try to download with urlretrieve")
+    print("item : \n%s" % result[2])
+    filename_item = (result[2]['filename'].split("/"))[-1]
+    try :
+        urlretrieve(result[2]['download_path'],filename_item)
+    except Exception as e:
+        print ("Issue downloading id_sitools_view : %s " % result[2]['id_sitools_view'])
+        print ("type is: %s" % e.__class__.__name__)
+        print ("Message : %s" % e.message)
+    else :
+        print("Download id_sitools_view : %s , file %s completed" % (result[2]['id_sitools_view'],filename_item))
+
+if __name__ == "__main__":
+    main()
