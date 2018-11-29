bb53a4a81b9163d59fcf972d0ccde80ce072afe1
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2018-11-29 14:31:14 +0100

    Solve pep8 issues

--- examples/example_gaia.py
+++ examples/example_gaia.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 """
+Test gaia-dem
+Chloe Guennou web interface
+Temp inversion
 
-@author: Pablo ALINGERY for IAS 28-08-2012
 """
+
+__author__ = "Pablo ALINGERY"
+
 from sitools2.clients.gaia_client_medoc import gaia_search, gaia_get
 from datetime import datetime, timedelta
 
@@ -13,26 +18,29 @@ gaia_data_list = gaia_search(DATES=[d1, d2], nb_res_max=10)
 for item in gaia_data_list:
     print(item)
 
-#the fastest way to retrieve data
+# the fastest way to retrieve data
 gaia_get(gaia_list=gaia_data_list, target_dir="results")
 
-#Need to get a tar ball do sthg like :
-#gaia_get(GAIA_LIST=gaia_data_list,DOWNLOAD_TYPE="tar", target_dir="results" ,FILENAME="my_dowload_file.tar")
+# Need to get a tar ball do sthg like :
+# gaia_get(GAIA_LIST=gaia_data_list,DOWNLOAD_TYPE="tar", target_dir="results" ,FILENAME="my_dowload_file.tar")
 
-#Need to do it quietly 
-#gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results",QUIET=True)
+# Need to do it quietly
+# gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results",QUIET=True)
 
-#specify TYPE you want to  retrieve , it should be in list 'temp','em','width','chi2' (TYPE=['all'] will do as well ), FILENAME would be the default one 
-#gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results", TYPE=['temp','em'])
-#Warning TYPE will be ignored if you specify DOWNLOAD_TYPE
+# specify TYPE you want to  retrieve , it should be in list 'temp','em','width','chi2' (TYPE=['all'] will do as well ),
+#  FILENAME would be the default one
+# gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results", TYPE=['temp','em'])
+# Warning TYPE will be ignored if you specify DOWNLOAD_TYPE
 
-#To specify FILENAME you want to retrieve, Use get_file() method 
-#FILENAME should be a dictionary with key within 'temp','em','width','chi2' and value can be whatever you want
-#for item in gaia_data_list :
-#	file_date_obs = (item.date_obs).strftime("%Y-%m-%dT%H:%M:%S")
-#	item.get_file(TARGET_DIR="results", FILENAME = {'temp' :"temp_%s.fits" % file_date_obs, 'em':"em_%s.fits" % file_date_obs})
+# To specify FILENAME you want to retrieve, Use get_file() method
+# FILENAME should be a dictionary with key within 'temp','em','width','chi2' and value can be whatever you want
+# for item in gaia_data_list:
+#    # file_date_obs = (item.date_obs).strftime("%Y-%m-%dT%H:%M:%S")
+#    # item.get_file(TARGET_DIR="results", FILENAME = {'temp' :"temp_%s.fits" % file_date_obs,
+#    # 'em':"em_%s.fits" % file_date_obs})
 
-#########################Warning###########################
-#specify both FILENAME and TYPE is not allowed 
-#gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results", FILENAME={'temp' :'temp.fits','em':'em.fits'}, TYPE=['temp','em'])
+# ########################Warning###########################
+# specify both FILENAME and TYPE is not allowed
+# gaia_get(GAIA_LIST=gaia_data_list, TARGET_DIR="results", FILENAME={'temp' :'temp.fits','em':'em.fits'},
+#  TYPE=['temp','em'])
 ###########################################################
