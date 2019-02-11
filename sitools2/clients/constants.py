f636515818cb4c2cb4c0559e2a4b2785394d4d5f
Pablo Alingery <pablo.alingery@ias.u-psud.fr>
2019-02-11 15:56:18 +0100

    Add sdo datasets id

--- sitools2/clients/constants.py
+++ sitools2/clients/constants.py
@@ -7,3 +7,6 @@ constants.py
 
 SITOOLS2_URL = 'http://idoc-medoc-test.ias.u-psud.fr'
 FUNCTIONAL_TEST = False
+SDO_DATASET_ID = 'webs_IAS_SDO_dataset'
+SDO_HMI_DATASET_ID = 'webs_IAS_SDO_HMI_dataset'
+SDO_AIA_DATASET_ID = 'webs_IAS_SDO_AIA_dataset'
